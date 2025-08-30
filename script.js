a2c37d9d6cf203557aa5d953eaf61a877958bab4
Thomas Beioley <31884223+mushola@users.noreply.github.com>
2025-08-30 20:27:44 +0100

    adds hover event to grid squares allowing draw functionality

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ let grid = []
 // container.appendChild(gridSquare);
 
 
-for (i=0; i<16*16; i++) {
+for (i=0; i < 16*16; i++) {
   let gridSquare = document.createElement("div");
   gridSquare.classList.add("grid-square");
   container.appendChild(gridSquare);
+  gridSquare.addEventListener("mouseover", function (e) {
+  draw(gridSquare);
+  })
   grid.push(gridSquare);
-  
+}
+
+function draw(square) {
+  square.classList.add("drawn-square");
 }
