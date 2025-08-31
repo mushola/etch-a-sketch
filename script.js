cde272fe552b4e7fbf257e0d5e0db5efe60134f0
Thomas Beioley <31884223+mushola@users.noreply.github.com>
2025-08-31 18:35:44 +0100

    Adds frame around grid and header

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const container = document.querySelector("#container");
 
-let grid = []
+
 // const gridSquare = document.createElement("div");
 // gridSquare.classList.add("grid-square");
 
 // container.appendChild(gridSquare);
 
-
-for (i=0; i < 16*16; i++) {
-  let gridSquare = document.createElement("div");
-  gridSquare.classList.add("grid-square");
-  container.appendChild(gridSquare);
-  gridSquare.addEventListener("mouseover", function (e) {
-  draw(gridSquare);
-  })
-  grid.push(gridSquare);
+function drawGrid (size) {
+  for (i=0; i < size*size; i++) {
+    let gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid-square");
+    container.appendChild(gridSquare);
+    gridSquare.addEventListener("mouseover", function (e) {
+    draw(gridSquare);
+    })
+  }
 }
 
 function draw(square) {
   square.classList.add("drawn-square");
 }
+
+drawGrid(16)
