74a51b0e0cc45b15a7f23d09eaca52d6074434b7
Thomas Beioley <31884223+mushola@users.noreply.github.com>
2025-09-28 12:33:08 +0100

    Adds resize and clear buttons and functionality Minor refactor of existing code

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+let gridSize = "640px";
+let squareDim = 20;
+let squareEdge = undefined
 const container = document.querySelector("#container");
 
+// set width of header div
+const header = document.querySelector("#header");
+header.style.width = gridSize
 
-// const gridSquare = document.createElement("div");
-// gridSquare.classList.add("grid-square");
+// set buttons
+const btnGridResize = document.querySelector("#btnGridResize");
+btnGridResize.addEventListener("click", function (e) {
+    resizeGrid();
+})
+const btnClearGrid = document.querySelector("#btnClearGrid");
+btnClearGrid.addEventListener("click", function (e) {
+    replaceGrid(squareDim);
+})
 
-// container.appendChild(gridSquare);
+// launch cgrid
+replaceGrid(squareDim);
 
-function drawGrid (size) {
+function placeGrid (size) {
+  // Creates size x size grid in container
+
+  // set container and square size
+  container.style.width = gridSize
+  squareEdge = parseInt(gridSize,10) / squareDim + "px"
+  
+  // place squares
   for (i=0; i < size*size; i++) {
     let gridSquare = document.createElement("div");
     gridSquare.classList.add("grid-square");
+    gridSquare.style.width = squareEdge
+    gridSquare.style.height = squareEdge
     container.appendChild(gridSquare);
     gridSquare.addEventListener("mouseover", function (e) {
     draw(gridSquare);
@@ -18,7 +41,31 @@ function drawGrid (size) {
 }
 
 function draw(square) {
+  // mouse drawing effect
   square.classList.add("drawn-square");
 }
 
-drawGrid(16)
+function deleteGrid() {
+  // deletes container contents
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+}
+
+function replaceGrid(dim) {
+  deleteGrid();
+  placeGrid(dim);
+}
+
+function resizeGrid() {
+  // resize grid prompt
+  let newDim = parseInt(prompt("New grid dimension (1-100):"), 10)
+  if(isNaN(newDim)) {
+    alert("Entry must be a number")
+  } else if (newDim > 100 || newDim < 1) {
+    alert("Entry must be between 1 and 100")
+  } else {
+    squareDim = newDim
+    replaceGrid(squareDim)
+  }
+}
